cea3e2eafaa595d1abf6b42f9959422811875c35
awebofbrown <ajcbrown820@gmail.com>
2024-07-09 10:50:36 +1000

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -14,26 +14,35 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer conn.Close()
 
-	pong := "+PONG\r\n"
-	res := []byte(pong)
-	reader := bufio.NewReader(conn)
 	for {
-		msg, err := reader.ReadString('\n')
+		conn, err := l.Accept()
 		if err != nil {
-			panic(err)
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
 		}
 
-		msg = strings.Trim(msg, "\r\n")
-		if msg == "PING" {
-			conn.Write(res)
-		}
+		go func(conn net.Conn) {
+			defer conn.Close()
+
+			pong := "+PONG\r\n"
+			res := []byte(pong)
+			reader := bufio.NewReader(conn)
+			for {
+				msg, err := reader.ReadString('\n')
+				if err != nil {
+					panic(err)
+				}
+
+				msg = strings.Trim(msg, "\r\n")
+
+				if msg == "EOF" {
+					return
+				}
+				if msg == "PING" {
+					conn.Write(res)
+				}
+			}
+		}(conn)
 	}
-
 }
